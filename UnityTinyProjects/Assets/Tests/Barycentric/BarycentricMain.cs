7723e6fc4cd1d02f96b2b02402ed9c33cbf8a1c7
lexnewgate <lexnewgate@outlook.com>
2023-07-04 01:09:02 +0800

    close #29 draw tri

--- UnityTinyProjects/Assets/Tests/Barycentric/BarycentricMain.cs
+++ UnityTinyProjects/Assets/Tests/Barycentric/BarycentricMain.cs
@@ -1,30 +1,39 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class BarycentricMain : MonoBehaviour
 {
-    public GameObject m_start;
+    public GameObject m_a;
 
-    public GameObject m_end;
+    public GameObject m_b;
+    public GameObject m_c;
     public GameObject m_player;
     public int m_count = 10;
     // Start is called before the first frame update
     void OnDrawGizmos()
     {
-        var startColor = m_start.GetComponent<MeshRenderer>().sharedMaterial.color;
-        var endColor = m_end.GetComponent<MeshRenderer>().sharedMaterial.color;
+        //draw tri abc
+        Gizmos.DrawLine(m_a.transform.position, m_b.transform.position);
+        Gizmos.DrawLine(m_b.transform.position, m_c.transform.position);
+        Gizmos.DrawLine(m_c.transform.position, m_a.transform.position);
         
-        Gizmos.DrawLine(m_start.transform.position, m_end.transform.position);
-        Gizmos.DrawLine(m_start.transform.position, m_player.transform.position);
-        Gizmos.DrawLine(m_player.transform.position, m_end.transform.position);
         
-        var start2End= m_end.transform.position - m_start.transform.position;
-        var start2Player= m_player.transform.position - m_start.transform.position;
-        var projScale = Vector3.Dot(start2End, start2Player)/start2End.sqrMagnitude;
-        var projPointOnStartEnd = m_start.transform.position + projScale * start2End;
-        Gizmos.color=(1-projScale)*startColor+projScale*endColor;
-        Gizmos.DrawCube(projPointOnStartEnd,Vector3.one*0.5f);
-        Handles.Label(projPointOnStartEnd+Vector3.up,projScale.ToString("0.00"));
+        
+        // var startColor = m_a.GetComponent<MeshRenderer>().sharedMaterial.color;
+        // var endColor = m_b.GetComponent<MeshRenderer>().sharedMaterial.color;
+        //
+        // Gizmos.DrawLine(m_a.transform.position, m_b.transform.position);
+        // Gizmos.DrawLine(m_a.transform.position, m_player.transform.position);
+        // Gizmos.DrawLine(m_player.transform.position, m_b.transform.position);
+        //
+        // var start2End= m_b.transform.position - m_a.transform.position;
+        // var start2Player= m_player.transform.position - m_a.transform.position;
+        // var projScale = Vector3.Dot(start2End, start2Player)/start2End.sqrMagnitude;
+        // var projPointOnStartEnd = m_a.transform.position + projScale * start2End;
+        // Gizmos.color=(1-projScale)*startColor+projScale*endColor;
+        // Gizmos.DrawCube(projPointOnStartEnd,Vector3.one*0.5f);
+        // Handles.Label(projPointOnStartEnd+Vector3.up,projScale.ToString("0.00"));
     }
 
     // Update is called once per frame
