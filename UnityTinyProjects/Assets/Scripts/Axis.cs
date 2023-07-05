f2ed85b5c606ae680f287fad7ffc5cfb6b3607bd
weiwuhao <weiwuhao@blackjack-inc.com>
2023-07-05 18:11:52 +0800

    update

---
+++ UnityTinyProjects/Assets/Scripts/Axis.cs
@@ -0,0 +1,13 @@
+﻿using Lex;
+using UnityEngine;
+
+public class Axis: MonoBehaviour
+{
+    public GameObject m_start;
+    public GameObject m_end;
+    
+    public void OnDrawGizmos()
+    {
+        Draw.Arrow(m_start.transform.position,m_end.transform.position);
+    }
+}
