b524add2c16bce0b868fdb5f0af748c76db28364
lexnewgate <lexnewgate@outlook.com>
2023-07-05 23:45:32 +0800

    close #34 close #36 close #37 draw axis v0

--- UnityTinyProjects/Assets/Scripts/Axis.cs
+++ UnityTinyProjects/Assets/Scripts/Axis.cs
@@ -1,22 +1,31 @@
 ﻿using Lex;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class Axis: MonoBehaviour
 {
+    public bool m_worldMode = true;
+    public int m_length = 1;
+    public float m_headScale = 0.1f;
+    public float m_arrowAngle=30f;
+    
     void OnDrawGizmos()
     {
+        if (!m_worldMode)
+        {
+            Gizmos.matrix=transform.localToWorldMatrix;
+        }
+        
         //up arrow
         Gizmos.color=Color.green;
-        Draw.DrawArrow(transform.position,transform.position+Vector3.up,0.1f,30f);
+        Draw.DrawArrow(transform.position,transform.position+Vector3.up*m_length,m_headScale,m_arrowAngle);
         
         //right arrow
         Gizmos.color=Color.red;
-        Draw.DrawArrow(transform.position,transform.position+Vector3.right,0.1f,30f);
+        Draw.DrawArrow(transform.position,transform.position+Vector3.right*m_length,m_headScale,m_arrowAngle);
         
         //forward arrow
         Gizmos.color=Color.blue;
-        Draw.DrawArrow(transform.position,transform.position+Vector3.forward,0.1f,30f);
+        Draw.DrawArrow(transform.position,transform.position+Vector3.forward*m_length,m_headScale,m_arrowAngle);
         
     }
 }
